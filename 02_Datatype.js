54b5e2d1b81a219277ba53683e37e52d964a946a
Devvishal12 <105010083+Devvishal12@users.noreply.github.com>
2025-03-28 11:20:59 +0000

    Data type

---
+++ 02_Datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version
+
+// alert() // we are using node.js, not browser
+
+let name = "vishal" // string Datatype
+let age = 23 // Inteser Data type
+let isLoggedIn = false // boolian Datatype
+
+/* number => Minimum Safe Integer: Number.MIN_SAFE_INTEGER (−(2⁵³ − 1) ≈ -9007199254740991)
+
+Maximum Safe Integer: Number.MAX_SAFE_INTEGER (2⁵³ − 1 ≈ 9007199254740991) */
+
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof age);
