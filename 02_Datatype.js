6d8f5c93f0aff6d9964f7e6359a1213404caf18f
Devvishal12 <105010083+Devvishal12@users.noreply.github.com>
2025-03-28 11:23:25 +0000

    Datatype

--- 02_Datatype.js
+++ 02_Datatype.js
@@ -21,4 +21,5 @@ Maximum Safe Integer: Number.MAX_SAFE_INTEGER (2⁵³ − 1 ≈ 9007199254740991
 // object
 
 
-console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
